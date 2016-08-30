424ffb45398f4297ad23fdfab7121d09b9756abb
shakhoond <shayan.akhoond@gmail.com>
2016-08-31 10:29:27 +1200

    scores a normal frame

---
+++ game.js
@@ -0,0 +1,8 @@
+module.exports = {
+	scoreFrame: scoreFrame
+}
+
+function scoreFrame(frame) {
+	var sum = frame[0] + frame[1]
+	return sum
+}
---
+++ tests/index.js
@@ -0,0 +1,17 @@
+var test = require('tape')
+var game = require('../game')
+
+
+test('scores a gutterball frame', function(t){
+	var frame = [0, 0]
+	var score = game.scoreFrame(frame)
+	t.equal(score, 0)
+	t.end()
+})
+
+test('scores a normal frame', function(t) {
+	var frame = [2, 3]
+	var score = game.scoreFrame(frame)
+	t.equal(score, 5)
+	t.end()
+}) 
